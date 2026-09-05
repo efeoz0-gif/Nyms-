f6a217cefa04c8882d7b23fc1a5fa835bb59cb13
Oz Sigorta <efeoz5530@gmail.com>
2026-09-05 12:31:53 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'nyms-v1';
+const ASSETS = [
+  'index.html',
+  'app.html',
+  'css/styles.css',
+  'js/firebase-config.js',
+  'js/auth.js',
+  'js/pwa-install.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
